0c50f24ed7559076d30367ee53d8d3ff242fc942
Hyperion60 <admin@hyperion.tf>
2024-07-03 19:50:19 +0200

    cmd: increase timeout Use longer timeout to avoid fatal errors.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os, time
 def main():
     t0 = time.time_ns()
     print(os.listdir())
-    print("Temps d'execution : {} ns".format(time.time_ns() - t0))
+    print("Temps d'exécution : {} ns".format(time.time_ns() - t0))
     return
 
 
